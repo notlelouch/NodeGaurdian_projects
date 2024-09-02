7aef0192656b62cda9f62db4f0cc473a5c389c01
notlelouch <notlelouchvb@gmail.com>
2024-09-02 12:48:29 +0530

    claws contract

--- campaigns/building-on-zksync/zksync-contracts/contracts/Claws.sol
+++ campaigns/building-on-zksync/zksync-contracts/contracts/Claws.sol
@@ -2,5 +2,28 @@
 pragma solidity ^0.8.19;
 
 contract Claws {
-    // TODO: Complete this contract
+    address public creator;
+
+    string _target;
+    
+    constructor () public {
+        creator = msg.sender;
+    }
+
+    modifier onlyCreator() {
+        require(msg.sender == owner, "Not the Contract creator!");
+    }
+
+    function slash(string memory _target) onlyCreator(){
+        _target = "slashed";
+    }
+
+    function isSlashed(string memory _target) external view returns (bool) {
+        if (_target == "slashed") {
+            return true;
+        } else {
+            return false;
+        }
+
+    }
 }
